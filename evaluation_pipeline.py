0d07f2864b31df2d3966d5beca92b446463d3ad2
ParthMunjalJoshi <parthmunjaljoshi@gmail.com>
2025-06-26 14:31:51 +0530

    DRY-Stage-1

--- evaluation_pipeline.py
+++ evaluation_pipeline.py
@@ -72,23 +72,21 @@ def classifier_helper(model,dataset_details):
 def create_ART_classifiers(classical_model, hybrid_model,dataset_details):
     return (classifier_helper(classical_model,dataset_details), classifier_helper(hybrid_model,dataset_details))
 
-#Generate 3-Adversarial Attacks on model
+#Generate Adversarial Attacks on model
 def generate_adversarial_attacks(classifier,x_test,epsilon=0.01,maximum_iterations=40):
     fgsm = FastGradientMethod(estimator=classifier, eps=epsilon)
     pgd = ProjectedGradientDescent(estimator=classifier, eps=epsilon, eps_step=(2 * epsilon) / maximum_iterations, max_iter=maximum_iterations, batch_size=32, verbose=True)
     cw = CarliniL2Method(classifier=classifier, targeted=False, learning_rate=0.01, max_iter=maximum_iterations, binary_search_steps=5, confidence=0.0, initial_const=0.01)
-    return (fgsm.generate(x_test),pgd.generate(x_test),cw.generate(x_test))
+    return (x_test,fgsm.generate(x_test),pgd.generate(x_test),cw.generate(x_test))
 
 #Generate Min/Mean Perturbation size for model
 def generate_pert(classifier,x_pgd,x_cw,x_test):
     min_perturbation_fgsm = empirical_robustness(classifier=classifier, x=x_test, attack_name="fgsm", attack_params={"eps": 0.1})
-    pgd_perturbations = np.linalg.norm((x_pgd - x_test).reshape(len(x_test), -1), axis=1)
-    avg_pgd_perturbation = np.mean(pgd_perturbations)
-    cw_perturbations = np.linalg.norm((x_cw - x_test).reshape(len(x_test), -1), axis=1)
-    avg_cw_perturbation = np.mean(cw_perturbations)
-    return (min_perturbation_fgsm,avg_pgd_perturbation,avg_cw_perturbation)
+    avg_pgd_perturbation = np.mean(np.linalg.norm((x_pgd - x_test).reshape(len(x_test), -1), axis=1))
+    avg_cw_perturbation = np.mean(np.linalg.norm((x_cw - x_test).reshape(len(x_test), -1), axis=1))
+    return (0,min_perturbation_fgsm,avg_pgd_perturbation,avg_cw_perturbation)
 
-#Evaluate model for a specific attack
+#Evaluate model for a specific attack.
 #Using weighted avg for precision,f1,auc-roc as well as OneVsRest strategy for auc-roc this is done to extend these to multi-class classification
 def evaluate_model(classifier,x_test,y_test):
     preds = classifier.predict(x_test)
@@ -122,58 +120,29 @@ def eval_pipeline(dataset_name,classical_model, hybrid_model, epsilon=0.01, maxi
     #Loading given dataset's test-set
     dataset_details,x_test,y_test = load_testset(dataset_name,subset_size)
     #Generating ART Classifiers
-    classical_classifier,hybrid_classifier = create_ART_classifiers(classical_model, hybrid_model,dataset_details)
-    #Generating Adversarial Examples
-    classical_x_fgsm,classical_x_pgd,classical_x_cw = generate_adversarial_attacks(classical_classifier,x_test,epsilon,maximum_iterations)
-    hybrid_x_fgsm,hybrid_x_pgd,hybrid_x_cw = generate_adversarial_attacks(hybrid_classifier,x_test,epsilon,maximum_iterations)
-    #Generating Perturbation sizes
-    classical_min_pert_fgsm, classical_avg_pert_pgd,classical_avg_pert_cw = generate_pert(classical_classifier,classical_x_pgd,classical_x_cw,x_test)
-    hybrid_min_pert_fgsm, hybrid_avg_pert_pgd,hybrid_avg_pert_cw = generate_pert(hybrid_classifier,hybrid_x_pgd,hybrid_x_cw,x_test)
-    #Measuring clean robustness metrics
-    classical_acc_clean,classical_precision_clean,classical_f1_clean,classical_auc_clean = evaluate_model(classical_classifier,x_test,y_test)
-    hybrid_acc_clean,hybrid_precision_clean,hybrid_f1_clean,hybrid_auc_clean = evaluate_model(hybrid_classifier,x_test,y_test)
-    #Measuring fgsm robustness metrics
-    classical_acc_fgsm,classical_precision_fgsm,classical_f1_fgsm,classical_auc_fgsm = evaluate_model(classical_classifier,classical_x_fgsm,y_test)
-    hybrid_acc_fgsm,hybrid_precision_fgsm,hybrid_f1_fgsm,hybrid_auc_fgsm = evaluate_model(hybrid_classifier,hybrid_x_fgsm,y_test)
-    #Measuring pgd robustness metrics
-    classical_acc_pgd,classical_precision_pgd,classical_f1_pgd,classical_auc_pgd = evaluate_model(classical_classifier,classical_x_pgd,y_test)
-    hybrid_acc_pgd,hybrid_precision_pgd,hybrid_f1_pgd,hybrid_auc_pgd = evaluate_model(hybrid_classifier,hybrid_x_pgd,y_test)
-    #Measuring Carlini-Wagner robustness metrics
-    classical_acc_cw,classical_precision_cw,classical_f1_cw,classical_auc_cw = evaluate_model(classical_classifier,classical_x_cw,y_test)
-    hybrid_acc_cw,hybrid_precision_cw,hybrid_f1_cw,hybrid_auc_cw = evaluate_model(hybrid_classifier,hybrid_x_cw,y_test)
-    #Measuring Classical -> Hybrid Transferability for FGSM attack
-    ch_fgsm_tsr, ch_fgsm_acc_drop = evaluate_transferability(hybrid_classifier,classical_x_fgsm,y_test,hybrid_acc_fgsm)
-    #Measuring Classical -> Hybrid Transferability for PGD attack
-    ch_pgd_tsr, ch_pgd_acc_drop = evaluate_transferability(hybrid_classifier,classical_x_pgd,y_test,hybrid_acc_pgd)
-    #Measuring Classical -> Hybrid Transferability for CW attack
-    ch_cw_tsr, ch_cw_acc_drop = evaluate_transferability(hybrid_classifier,classical_x_cw,y_test,hybrid_acc_cw)
-    #Measuring Hybrid -> Classical Transferability for FGSM attack
-    hc_fgsm_tsr, hc_fgsm_acc_drop = evaluate_transferability(classical_classifier,hybrid_x_fgsm,y_test,classical_acc_fgsm)
-    #Measuring Hybrid -> Classical Transferability for PGD attack
-    hc_pgd_tsr, hc_pgd_acc_drop = evaluate_transferability(classical_classifier,hybrid_x_pgd,y_test,classical_acc_pgd)
-    #Measuring Hybrid -> Classical Transferability for CW attack
-    hc_cw_tsr, hc_cw_acc_drop = evaluate_transferability(classical_classifier,hybrid_x_cw,y_test,classical_acc_cw)
-    #Creating Dataframe for Robustness Data
-    classical_hash = generate_sha3_256_hash(classical_model)
-    hybrid_hash = generate_sha3_256_hash(hybrid_model)
-    robustness_data = {
-        "CNN_ID": 4*[classical_hash]+4*[hybrid_hash],
-        "attack_type": ['clean','fgsm','pgd','cw']*2,
-        "accuracy":[classical_acc_clean,classical_acc_fgsm,classical_acc_pgd,classical_acc_cw,hybrid_acc_clean,hybrid_acc_fgsm,hybrid_acc_pgd,hybrid_acc_cw],
-        "precision":[classical_precision_clean,classical_precision_fgsm,classical_precision_pgd,classical_precision_cw,hybrid_precision_clean,hybrid_precision_fgsm,hybrid_precision_pgd,hybrid_precision_cw],
-        "f1_score":[classical_f1_clean,classical_f1_fgsm,classical_f1_pgd,classical_f1_cw,hybrid_f1_clean,hybrid_f1_fgsm,hybrid_f1_pgd,hybrid_f1_cw],
-        "auc_roc":[classical_auc_clean,classical_auc_fgsm,classical_auc_pgd,classical_auc_cw,hybrid_auc_clean,hybrid_auc_fgsm,hybrid_auc_pgd,hybrid_auc_cw],
-        "perturbation_type":['null','min','avg','avg']*2,
-        "perturbation_size":[0.0,classical_min_pert_fgsm,classical_avg_pert_pgd,classical_avg_pert_cw,0.0,hybrid_min_pert_fgsm,hybrid_avg_pert_pgd,hybrid_avg_pert_cw]
-    }
-    robustness_dataframe = pd.DataFrame(robustness_data)
-    #Creating Dataframe for Transferability Data
-    transferability_data = {
-        "CNN_Donor_ID":[classical_hash]*3 + [hybrid_hash]*3,
-        "CNN_Recipient_ID":[hybrid_hash]*3 + [classical_hash]*3,
-        "attack_type":['fgsm','pgd','cw']*2,
-        "TSR":[ch_fgsm_tsr,ch_pgd_tsr,ch_cw_tsr,hc_fgsm_tsr,hc_pgd_tsr,hc_cw_tsr],
-        "acc_drop":[ch_fgsm_acc_drop,ch_pgd_acc_drop,ch_cw_acc_drop,hc_fgsm_acc_drop,hc_pgd_acc_drop,hc_cw_acc_drop]
-    }
-    transferability_dataframe = pd.DataFrame(transferability_data)
-    return (robustness_dataframe,transferability_dataframe)
+    classifiers = create_ART_classifiers(classical_model, hybrid_model,dataset_details)
+    #Measuring Robustness
+    robustness_data = []
+    attacks = ["clean","fgsm","pgd","cw"]
+    perts = ["none","min","mean","mean"]
+    hashs_set, all_adversarial_examples, all_accuracies = [], [], [] 
+    for classifier in classifiers:
+        hash_value = generate_sha3_256_hash(classifier.model)
+        hashs_set.append(hash_value)
+        adv_examples = generate_adversarial_attacks(classifier,x_test,epsilon,maximum_iterations)
+        all_adversarial_examples.append(adv_examples)
+        perturbations = generate_pert(classifier,adv_examples[2],adv_examples[3],x_test)
+        for idx,(adv_example,perturbation) in enumerate(zip(adv_examples,perturbations)):
+            metrics = evaluate_model(classifier,adv_example,y_test)
+            all_accuracies.append(metrics[0])
+            robustness_data.append([hash_value,attacks[idx]]+list(metrics)+[perts[idx],perturbation])
+    robustness_dataframe = pd.DataFrame(robustness_data,columns=["CNN_ID","attack_type","acc","precision","f1","auc-roc","pert_type","pert_size"])
+    transfer_data = []
+    for i, attack in enumerate(attacks[1:]):
+        metrics = evaluate_transferability(classifiers[1], all_adversarial_examples[0][i+1], y_test, all_accuracies[5+i])
+        transfer_data.append([hashs_set[0], hashs_set[1], attack]+list(metrics))
+        metrics2 = evaluate_transferability(classifiers[0], all_adversarial_examples[1][i+1], y_test, all_accuracies[1+i])
+        transfer_data.append([hashs_set[1], hashs_set[0], attack]+list(metrics2))
+    transfer_dataframe = pd.DataFrame(transfer_data,columns=["Donor_ID","Recipient_ID","attack_type","TSR","acc_drop"])
+    return robustness_dataframe,transfer_dataframe
+    
