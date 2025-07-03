33c5db7d5133a32f14b181ebde75a97c43f8a27d
ParthMunjalJoshi <parthmunjaljoshi@gmail.com>
2025-07-03 15:06:41 +0530

    carliniwagner disabled by default - bug fix

--- entanglement_experiment_homogenous.py
+++ entanglement_experiment_homogenous.py
@@ -37,11 +37,15 @@ def main():
         "cifar10":(32,32,3)
     }
     column_names = ["model","hash"]
-    os.makedirs(os.path.dirname("models"), exist_ok=True)
+    os.makedirs("models", exist_ok=True)
     for dataset in datasets:
-        classical_model = fac.entanglement_model_factory(shapes[dataset],5,1,1,["classical"])
-        classical_model, _ = tm.train_model(classical_model,dataset)
-        classical_model.save("models/classical_"+dataset+".keras")
+        classical_path = "models/classical_"+dataset+".keras" 
+        if os.path.exists(classical_path):
+            classical_model = tf.keras.models.load_model(classical_path)   
+        else:
+            classical_model = fac.entanglement_model_factory(shapes[dataset],5,1,1,["classical"])
+            classical_model, _ = tm.train_model(classical_model,dataset)
+            classical_model.save(classical_path)
         entanglement_strategies = [
             ["none"]*3,
             ["linearx"]*3,
@@ -54,16 +58,20 @@ def main():
             ["staggeredz"]*3
         ]
         for entanglement_strategy in entanglement_strategies:
+            hybrid_path = "models/hybrid_"+dataset+str(entanglement_strategy)+".weights.h5"
             hybrid_model = fac.entanglement_model_factory(shapes[dataset],5,4,3,entanglement_strategy)
-            hybrid_model, _ = tm.train_model(hybrid_model,dataset)
-            hybrid_model.save_weights("models/hybrid_"+dataset+entanglement_strategy+".weights.h5")
+            if os.path.exists(hybrid_path):
+                hybrid_model.load_weights(hybrid_path,skip_mismatch=True)
+            else:
+                hybrid_model, _ = tm.train_model(hybrid_model,dataset)
+                hybrid_model.save_weights(hybrid_path)
             rb,tf = ep.eval_pipeline(dataset,classical_model,hybrid_model,0.01,40,1000)
             append_to_db("lib/robustness.csv",rb)
             append_to_db("lib/transferability.csv",tf)
-            index.append([["CNN_"+dataset] , rb["CNN_ID"].unique()[0]])
-            index.append([["HQCNN_"+dataset+"_"+str(entanglement_strategy)] , rb["CNN_ID"].unique()[1]])
-            index_df = pd.DataFrame(index,columns=column_names)
-            append_to_db("lib/index.csv",index_df)
+            index.append(["CNN_"+dataset , rb["CNN_ID"].unique()[0]])
+            index.append(["HQCNN_"+dataset+"_"+str(entanglement_strategy) , rb["CNN_ID"].unique()[1]])
+        index_df = pd.DataFrame(index,columns=column_names)
+        append_to_db("lib/index.csv",index_df)
 
 if __name__ == "__main__":
     main()
